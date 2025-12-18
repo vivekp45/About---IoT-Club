9f33b015ceec6c7e231205b9c595ce5a2e433f0f
Vivek Pate <vivekpate00s@gmail.com>
2025-12-18 14:07:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ const buttons = document.querySelectorAll(".domain-btn");
 buttons.forEach(btn => {
   btn.addEventListener("click", () => {
     const content = btn.nextElementSibling;
-    const arrow = btn.querySelector(".arrow");
 
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
-      arrow.classList.remove("open");
+      btn.querySelector("span").textContent = "▼";
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-      arrow.classList.add("open");
+      btn.querySelector("span").textContent = "▲";
     }
   });
 });
